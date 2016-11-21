404d74e0652d4938d5cba2db7e69f4c6f52a0e04
GMaiolo <guille.maiolo@gmail.com>
2016-11-21 19:50:28 -0300

    added execution on search input keystroke, as a new search is triggered with each one

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 init();
 
-function init() {
-    removeW3Results();
-    /* handling searching again from the results page */
-    window.document.body.onchange = function() {
-        setTimeout(removeW3Results, 800);
-    }
-}
-
 function removeW3Results(){
-    /* avoiding google new tab page, etc */
-    if(window.location.pathname !== '/search') return;
     var allW3Links = allW3Links();
-    console.info(allW3Links.length + ' W3Schools links will be removed from this search.');
     allW3Links.forEach(deleteOldGrandpaNode);
+    if(allW3Links.length) console.info(allW3Links.length + ' W3Schools links were removed from this search.');
 
     function allW3Links() {
         var resultLinksQuery = 'div.g .r > a';
@@ -34,5 +24,25 @@ function removeW3Results(){
     function isFromW3(link) {
         return link.getAttribute('href').indexOf('w3schools') > 0;
     }
+}
 
+function init() {
+    /* avoiding google new tab page, etc */
+    if(window.location.pathname !== '/search') return;
+    attachEvents();
+    delayedRemoveW3Results();
+}
+
+function attachEvents() {
+    var searchInputSelector = 'lst-ib';
+    /* handling searching again from the results page */
+    window.document.body.addEventListener('change', delayedRemoveW3Results);
+    /* search input, as a new search is triggered on every keystroke */
+    var searchInput = document.getElementById(searchInputSelector);
+    searchInput.addEventListener('keypress', delayedRemoveW3Results);
+    /* new page */
+}
+
+function delayedRemoveW3Results() {
+    setTimeout(removeW3Results, 800);
 }
